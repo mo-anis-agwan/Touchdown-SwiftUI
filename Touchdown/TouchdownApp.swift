d7d41944af40abedbca08467dd069c15f9246e65
Mohmed Anis Agwan <anishagwan123@gmail.com>
2021-05-22 14:57:37 +0530

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Anis on 22/05/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
