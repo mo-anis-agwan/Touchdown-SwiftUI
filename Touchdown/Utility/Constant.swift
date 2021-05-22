bbc926d173e0eb4f801078a13d9d33e708804615
Mohmed Anis Agwan <anishagwan123@gmail.com>
2021-05-22 15:45:44 +0530

    finished setting up the assets and organization of the files, created teh footer view with the copyrights sections

---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,28 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Anis on 22/05/21.
+//
+
+import SwiftUI
+
+// Data
+
+// Color
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// Layout
+
+// UX
+
+// Api
+
+// Image
+
+// font
+
+// String
+
+// Misc
---
+++ Touchdown/View/Home/FooterView.swift
@@ -0,0 +1,40 @@
+//
+//  FooterView.swift
+//  Touchdown
+//
+//  Created by Anis on 22/05/21.
+//
+
+import SwiftUI
+
+struct FooterView: View {
+    var body: some View {
+        VStack(alignment: .center, spacing: 10, content: {
+            Text("We offer the most cutting edge, comfortable, lightweight and durable football helmets in the market at affordable prices.")
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .layoutPriority(2)
+            
+            Image("logo-lineal")
+                .renderingMode(.template)
+                .foregroundColor(.gray)
+                .layoutPriority(0)
+            
+            Text("Copyright © Childish El-Niño\nAll rights reserved")
+                .font(.footnote)
+                .fontWeight(.bold)
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .layoutPriority(1)
+        })
+        .padding()
+    }
+}
+
+struct FooterView_Previews: PreviewProvider {
+    static var previews: some View {
+        FooterView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
